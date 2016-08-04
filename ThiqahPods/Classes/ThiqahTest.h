22bf7734050a09c530873acfc0e583a7a57b5539
nwios <nawafd@outlook.sa>
2016-08-04 12:22:24 +0300

    create branch 0.1.0

---
+++ ThiqahPods/Classes/ThiqahTest.h
@@ -0,0 +1,13 @@
+//
+//  ThiqahTest.h
+//  ThiqahPods
+//
+//  Created by NAWAF DOSSARY on 8/4/16.
+//  Copyright © 2016 nwios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThiqahTest : NSObject
+
+@end
---
+++ ThiqahPods/Classes/ThiqahTest.m
@@ -0,0 +1,13 @@
+//
+//  ThiqahTest.m
+//  ThiqahPods
+//
+//  Created by NAWAF DOSSARY on 8/4/16.
+//  Copyright © 2016 nwios. All rights reserved.
+//
+
+#import "ThiqahTest.h"
+
+@implementation ThiqahTest
+
+@end
